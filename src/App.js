83a65eaddb72bb426458a5cd04280c928de8e424
Daan <daan.van.den.berg@tmogroup.asia>
2019-02-17 13:00:05 +0800

    CHANGE: Save state to local storage and fetch initial data on app load

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import isEmpty from 'lodash/isEmpty';
+import isUndefined from 'lodash/isUndefined';
+
+import { handleInitialData } from './actions/common';
+
+import { loadState } from './utils/localStorage';
 
 class App extends Component {
+
+	componentDidMount () {
+		const persistedState = loadState();
+		if (isEmpty(persistedState) || isUndefined(persistedState)) {
+			this.props.dispatch(handleInitialData());
+		}
+	}
+
 	render () {
 		return (
 			<div className='App'>
@@ -10,4 +25,4 @@ class App extends Component {
 	}
 }
 
-export default App;
+export default connect()( App );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import throttle from 'lodash/throttle';
 
 import configureStore from './store';
+import { loadState, saveState } from './utils/localStorage';
+
 import App from './App';
 
+const persistedState = loadState();
 const store = configureStore();
+store.subscribe(throttle(() => {
+	saveState({
+		questions: store.getState().questions,
+		users: store.getState().users,
+		authedUser: store.getState().authedUser
+	});
+}, 1000));
+
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
