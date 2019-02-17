c984ff2e04b6cb11424040cb22b0b4c6150409c7
Daan <daan.van.den.berg@tmogroup.asia>
2019-02-17 13:00:58 +0800

    ETC: Enable uikit framework and load Asap font via Google Fonts

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import throttle from 'lodash/throttle';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+import WebFont from 'webfontloader';
 
 import configureStore from './store';
 import { loadState, saveState } from './utils/localStorage';
 
 import App from './App';
+UIkit.use(Icons);
+
+WebFont.load({
+	google: {
+		families: ['Asap:400,400i,500,500i,600,600i,700,700i', 'sans-serif']
+	}
+});
 
 const persistedState = loadState();
 const store = configureStore();
